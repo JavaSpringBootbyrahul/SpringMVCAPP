ded3345981a865254225f7c2331c5c812ab49aa2
RAHUL-PC\Rzope <rzope5923@gmail.com>
2023-02-18 11:31:52 +0530

    'demoSpringMVCApp'

---
+++ src/main/java/com/muktai/MyController.java
@@ -0,0 +1,15 @@
+package com.muktai;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MyController 
+{
+	
+	@GetMapping("/")
+	public String homepage()
+	{
+		return "homepage";
+	}
+}
